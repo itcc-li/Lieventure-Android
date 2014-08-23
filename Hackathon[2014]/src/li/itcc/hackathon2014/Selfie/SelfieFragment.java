8f120c0e760bf1ad198b8d1b21378bf6e727c3af
thomasE1993 <thomas.elsensohn@gmail.com>
2014-08-24 00:16:01 +0200

    update

--- Hackathon[2014]/src/li/itcc/hackathon2014/Selfie/SelfieFragment.java
+++ Hackathon[2014]/src/li/itcc/hackathon2014/Selfie/SelfieFragment.java
@@ -3,11 +3,7 @@ package li.itcc.hackathon2014.Selfie;
 
 import li.itcc.hackathon2014.AbstractTourFragment;
 import li.itcc.hackathon2014.R;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -50,37 +46,10 @@ public class SelfieFragment extends AbstractTourFragment implements OnClickListe
         Logic x = new Logic(this);
         paths = x.TakePictureIntent();
     }
-
+    
     public void generate_bmp() {
         //ERROR Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pic1);
     }
 
-    // STAMP ITCC hinzufuegen
-    public void stamp(Bitmap pic, Bitmap watermark) {
-        // definieren .. pfad zum bitmap
-        Bitmap bitmap1 = pic;
-        Bitmap bitmap2 = watermark;
-        Bitmap resultBitmap = Bitmap.createBitmap(bitmap1.getWidth(), bitmap1.getHeight(),
-                Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(resultBitmap);
-        c.drawBitmap(bitmap1, 0, 0, null);
-        Paint p = new Paint();
-        p.setAlpha(127);
-        c.drawBitmap(bitmap2, 0, 0, p);
-    } // Funktion ende
-      // TO DO: bmp 2 jpeg und speichern
-
-    protected void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
-        canvas.drawColor(Color.GRAY);
-        // you need to insert some image flower_blue into res/drawable folder
-        //ERROR Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.Selfie);
-        // Best of quality is 80 and more, 3 is very low quality of image
-        //ERROR: Bitmap bJPGcompress = codec(b, Bitmap.CompressFormat.JPEG, 80);
-        // get dimension of bitmap getHeight() getWidth()
-        //ERROR int h = b.getHeight();
-        //ERROR canvas.drawBitmap(b, 10, 10, paint); // <-- grösse des Bitmaps?
-        //ERROR canvas.drawBitmap(bJPGcompress, 10, 10 + h + 10, paint);
-    }
-
+    // watermark ITCC hinzufuegen
 }
