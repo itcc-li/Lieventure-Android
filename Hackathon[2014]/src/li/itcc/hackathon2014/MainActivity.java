872d25af373ce0651cce0f53d39b162b3f919d3a
thomasE1993 <thomas.elsensohn@gmail.com>
2014-08-23 11:00:38 +0200

    test2

--- Hackathon[2014]/src/li/itcc/hackathon2014/MainActivity.java
+++ Hackathon[2014]/src/li/itcc/hackathon2014/MainActivity.java
@@ -17,7 +17,7 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-//test
+
 public class MainActivity extends Activity implements
 		NavigationDrawerFragment.NavigationDrawerCallbacks {
 
