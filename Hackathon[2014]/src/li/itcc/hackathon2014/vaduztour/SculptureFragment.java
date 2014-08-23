ad653282bc3140c76ff0083f91e19f924185ebce
Dario <Dario@10.1.8.62>
2014-08-23 23:25:16 +0200

    - eventlistener für gps - gpslistener interface

--- Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/SculptureFragment.java
+++ Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/SculptureFragment.java
@@ -1,32 +1,41 @@
 package li.itcc.hackathon2014.vaduztour;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import li.itcc.hackathon2014.AbstractTourFragment;
 import li.itcc.hackathon2014.R;
-import li.itcc.hackathon2014.vaduztour.sculpture.LocationListeners;
+import li.itcc.hackathon2014.vaduztour.hotcold.GPSLocationListener;
+import li.itcc.hackathon2014.vaduztour.sculpture.GPSHandler;
 import li.itcc.hackathon2014.vaduztour.sculpture.Tilt;
 
-public class SculptureFragment extends AbstractTourFragment implements SensorEventListener {
+public class SculptureFragment extends AbstractTourFragment implements SensorEventListener, GPSLocationListener {
     private SensorManager mSensorManager;
     private Sensor mGravitySensor;
-    private Button btnMeasurment;
+    private Button mBtnMeasurment;
+    private TextView mTextView;
+    private TextView mTextView_Tilt;
     private Tilt mCurrentTilt;
-    private LocationManager mLocationManager;
-    private LocationListeners mLocationListener;
+    private GPSHandler mGPSHandler;
     
-    private final float METER_PER_DEZIMALGRAD = 111319; // umrechnenung von gps daten in meter
+    private final float METER_PER_DEZIMALGRAD_OW = 78714; // umrechnenung von gps daten in meter (ost west richtung)
+    private final float METER_PER_DEZIMALGRAD_NS = 111319; // umrechnenung von gps daten in meter (nord süd richtung)
     private final float DELTA_ABSTAND_METER = 60; // meter
     private final float DELTA_TILT = 0.1f; // in grad
     
@@ -49,7 +58,6 @@ public class SculptureFragment extends AbstractTourFragment implements SensorEve
      */
     public SculptureFragment() {
         mCurrentTilt = new Tilt();
-        mLocationListener = new LocationListeners(); 
     }
 
     @Override
@@ -64,49 +72,31 @@ public class SculptureFragment extends AbstractTourFragment implements SensorEve
     public void onStart() {
         super.onStart();
         // init ui elements
-        btnMeasurment = (Button)getActivity().findViewById(R.id.btnMeasurement);
+        mBtnMeasurment = (Button)getActivity().findViewById(R.id.btnMeasurement);
+        mTextView = (TextView)getActivity().findViewById(R.id.textView2);
+        mTextView_Tilt = (TextView)getActivity().findViewById(R.id.textView3);
         
         // init sensores
         mSensorManager = (SensorManager)getActivity().getSystemService(getActivity().SENSOR_SERVICE);
         mGravitySensor = mSensorManager.getSensorList(Sensor.TYPE_GRAVITY).get(0);
-        mLocationManager = (LocationManager)getActivity().getSystemService(getActivity().LOCATION_SERVICE);        
- 
-        // TODO: check is gps enabled
-        initListener();
+        mGPSHandler = new GPSHandler(getActivity());
+                
+        initListener();        
     }
-   
-    /**
-     * check if all inputs correct
-     */
-    private void checkSculpture() {
-        boolean isNearbyCorrect = isNearbyCorrect();
-        boolean isTiltCorrect = isTiltCorrect();
-        
-        if (isNearbyCorrect && isTiltCorrect) {
-            Toast.makeText(getActivity(), "korrekt", Toast.LENGTH_LONG).show();
-        } else {
-            if (!isNearbyCorrect) {
-                Toast.makeText(getActivity(), "Position nicht korrekt", Toast.LENGTH_LONG).show();
-            }
-            if (!isTiltCorrect) {
-                Toast.makeText(getActivity(), "Neigung nicht korrekt", Toast.LENGTH_LONG).show();
-            }   
-        }
-    }
-    
+       
     /**
      * check if user close to target
      * @return true|false
      */
-    private boolean isNearbyCorrect() {
-        Location location = mLocationListener.getLocation();
-        
+    private boolean isNearbyByLocation(Location location) {        
         // calculate the different between ist/soll
         if (location != null) {
             double deltaX = SOLUTION_LOCATION_LONGITUDE - location.getLongitude();
             double deltaY = SOLUTION_LOCATION_LATITUDE - location.getLatitude();
             
-            double diff = Math.sqrt(Math.pow((deltaX * METER_PER_DEZIMALGRAD),2)  + Math.pow((deltaY * METER_PER_DEZIMALGRAD),2));
+            mTextView.setText("Lat: " + location.getLatitude() + "; Long: " + location.getLongitude());
+            
+            double diff = Math.sqrt(Math.pow((deltaX * METER_PER_DEZIMALGRAD_OW),2)  + Math.pow((deltaY * METER_PER_DEZIMALGRAD_NS),2));
             
             if (diff - DELTA_ABSTAND_METER <= 0.0) {
                 return true;
@@ -120,10 +110,13 @@ public class SculptureFragment extends AbstractTourFragment implements SensorEve
      * @return true|false
      */
     private boolean isTiltCorrect() {
+        boolean isCorrect = false;
         if (Math.abs((SOLUTION_TILT - mCurrentTilt.getZ()) / 90) < DELTA_TILT) {
-            return true;
+            isCorrect = true;
         }
-        return false;
+        mTextView_Tilt.setText("Tilt: " + mCurrentTilt.getZ());
+        
+        return isCorrect;
     }
     
     /**
@@ -132,12 +125,12 @@ public class SculptureFragment extends AbstractTourFragment implements SensorEve
     private void initListener() {
         mSensorManager.registerListener(this, mGravitySensor, SensorManager.SENSOR_DELAY_NORMAL);
         // TODO: change to gps
-        mLocationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, mLocationListener, null);
-        
-        btnMeasurment.setOnClickListener(new OnClickListener() {
+        //mLocationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, mLocationListener, null);
+               
+        mBtnMeasurment.setOnClickListener(new OnClickListener() {
             @Override     
             public void onClick(View v) {
-                checkSculpture();
+                mGPSHandler.startDelivery(SculptureFragment.this);
             }
         });
     }
@@ -167,5 +160,33 @@ public class SculptureFragment extends AbstractTourFragment implements SensorEve
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onLocation(Location location) {
+        boolean isNearbyCorrect = isNearbyByLocation(location);
+        boolean isTiltCorrect = isTiltCorrect();
+        
+        if (isNearbyCorrect && isTiltCorrect) {
+            Toast.makeText(getActivity(), "korrekt", Toast.LENGTH_LONG).show();
+        } else {
+            if (!isNearbyCorrect) {
+                Toast.makeText(getActivity(), "Position nicht korrekt", Toast.LENGTH_LONG).show();
+            }
+            if (!isTiltCorrect) {
+                Toast.makeText(getActivity(), "Neigung nicht korrekt", Toast.LENGTH_LONG).show();
+            }   
+        }
+    }
+
+    @Override
+    public void onLocationSensorEnabled() {
+    }
+
+    @Override
+    public void onLocationSensorDisabled() {
+        Toast.makeText(getActivity(), "GPS nicht aktiviert", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
     }
 }
---
+++ Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/sculpture/GPSHandler.java
@@ -0,0 +1,70 @@
+package li.itcc.hackathon2014.vaduztour.sculpture;
+
+import li.itcc.hackathon2014.vaduztour.hotcold.GPSLocationListener;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class GPSHandler implements LocationListener {
+    private Context mContext;
+    private GPSLocationListener mListener;
+    private LocationManager mLocationManager;
+    private Location mLocation;
+    private boolean isGPSEnabled;
+    
+    public GPSHandler(Context context) {
+        mContext = context;
+    }
+    
+    public void startDelivery(GPSLocationListener listener) {
+        if (listener == null) {
+            throw new NullPointerException();
+        }
+        mListener = listener;
+        
+        mLocationManager = (LocationManager) mContext
+                .getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        isGPSEnabled = true;
+    }
+
+    public void stopDelivery() {
+        if (mListener != null) {
+            mLocationManager.removeUpdates(this);
+            mListener = null;
+        }
+    }
+    
+    @Override
+    public void onLocationChanged(Location location) {
+        if (mListener != null) {
+            mListener.onLocation(location);
+        }
+        stopDelivery();        
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+       isGPSEnabled = true;
+       mListener.onLocationSensorDisabled();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) { 
+        isGPSEnabled = false;
+        mListener.onLocationSensorDisabled();
+    }
+    
+    public boolean getGPSEnabled() {
+        return isGPSEnabled;
+    }
+    
+}
