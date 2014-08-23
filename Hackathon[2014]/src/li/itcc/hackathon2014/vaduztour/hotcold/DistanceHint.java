4c7b714fd14f52865100c6670995e103c04ae8b1
simon.meier <simon.a.meier@outlook.com>
2014-08-23 14:11:48 +0200

    API defined

---
+++ Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/hotcold/DistanceHint.java
@@ -0,0 +1,12 @@
+package li.itcc.hackathon2014.vaduztour.hotcold;
+
+public enum DistanceHint {
+    COLD, WARM, HOT, ATDESTINATION, WORMER, HOTTER, COLDER;
+    private String speechText;
+    
+    //CONSTRUCTOR
+    
+    public String getSpeechRessource() {
+        return "Test";
+    }
+}
---
+++ Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/hotcold/GPSLocationListener.java
@@ -0,0 +1,7 @@
+package li.itcc.hackathon2014.vaduztour.hotcold;
+
+import android.location.Location;
+
+public interface GPSLocationListener {
+    public void onLocation(Location location);
+}
