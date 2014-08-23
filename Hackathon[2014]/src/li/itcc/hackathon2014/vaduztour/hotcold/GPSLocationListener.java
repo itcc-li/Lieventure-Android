90a7d0701164aa20163b7480d8b2ebed15a80503
Arthur <Arthur@10.1.3.24>
2014-08-23 15:23:36 +0200

    Add gps deliverer

--- Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/hotcold/GPSLocationListener.java
+++ Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/hotcold/GPSLocationListener.java
@@ -1,7 +1,14 @@
+
 package li.itcc.hackathon2014.vaduztour.hotcold;
 
 import android.location.Location;
 
 public interface GPSLocationListener {
+
     public void onLocation(Location location);
+
+    public void onLocationSensorEnabled();
+
+    public void onLocationSensorDisabled();
+
 }
