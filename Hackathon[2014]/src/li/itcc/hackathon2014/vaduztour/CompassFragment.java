610c5ca97cce803c05ff5eabd190a2166c4cf60a
Christoph <Christoph@NexusMobile>
2014-08-24 15:38:06 +0200

    Added images for compass.

--- Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/CompassFragment.java
+++ Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/CompassFragment.java
@@ -5,13 +5,16 @@ import li.itcc.hackathon2014.AbstractTourFragment;
 import li.itcc.hackathon2014.R;
 import li.itcc.hackathon2014.vaduztour.compass.CanvasView;
 import li.itcc.hackathon2014.vaduztour.compass.GPSTracker;
-import android.graphics.Color;
+
+import org.w3c.dom.Text;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CompassFragment extends AbstractTourFragment implements OnClickListener {
@@ -41,13 +44,16 @@ public class CompassFragment extends AbstractTourFragment implements OnClickList
         
         setNextButton(nextButton);
         
-        Button clickBtn = (Button)rootView.findViewById(R.id.test_button);
+        /*Button clickBtn = (Button)rootView.findViewById(R.id.test_button);
         clickBtn.setOnClickListener(this);
         
         Button distanceBtn = (Button)rootView.findViewById(R.id.distance_button);
-        distanceBtn.setOnClickListener(this);
-        
+        distanceBtn.setOnClickListener(this);*/
+
         drawView = (CanvasView)rootView.findViewById(R.id.compass_view);
+        TextView distanceView = (TextView)rootView.findViewById(R.id.distance_text);
+        
+        drawView.setDistanceView(distanceView);
         
         return rootView;
     }
@@ -55,6 +61,27 @@ public class CompassFragment extends AbstractTourFragment implements OnClickList
     @Override
     public void onClick(View v) {
         // TODO Auto-generated method stub
+        
+        // tracker.getLocation().bearingTo(tracker.createLocation(47.1491993, 9.5174042));
+
+//        SensorManager.getOrientation
+//        float azimuth = // get azimuth from the orientation sensor (it's quite simple)
+//        Location currentLoc = tracker.getLocation();
+//        
+//        azimuth = Math.toDegrees(azimuth);
+//        
+//        // convert radians to degrees
+//        azimuth = Math.toDegrees(azimuth);
+//        GeomagneticField geoField = new GeomagneticField(
+//                         (float) currentLoc.getLatitude(),
+//                         (float) currentLoc.getLongitude(),
+//                         (float) currentLoc.getAltitude(),
+//                         System.currentTimeMillis());
+//        
+//        azimuth += geoField.getDeclination(); // converts magnetic north into true north
+//        float bearing = currentLoc.bearingTo(target); // (it's already in degrees)
+//        float direction = azimuth - bearing;
+        
         
         this.tracker = new GPSTracker(getActivity());
         
@@ -66,12 +93,13 @@ public class CompassFragment extends AbstractTourFragment implements OnClickList
         }
         
         switch (v.getId()) {
-            case R.id.test_button:
+            /*case R.id.test_button:
                 Toast.makeText(getActivity(), "current lat: " + Double.toString(tracker.getLatitude()) + " current long: " + Double.toString(tracker.getLongitude()), 150).show();
                 break;
             case R.id.distance_button:
-                Toast.makeText(getActivity(), "Current distance: " + distance + "m", 150).show();
-                break;
+                // Toast.makeText(getActivity(), "bt: " + bt, 150).show();
+                // Toast.makeText(getActivity(), "Current distance: " + distance + "m", 150).show();
+                break;*/
         }
     }
 }
