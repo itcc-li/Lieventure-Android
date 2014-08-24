8ade09126be5a1550f1f3542a37fe82bc76dc985
Arthur <Arthur@ArthursHackPC>
2014-08-24 10:06:20 +0200

    Styles eingefügt

--- Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/hotcold/DistanceHint.java
+++ Hackathon[2014]/src/li/itcc/hackathon2014/vaduztour/hotcold/DistanceHint.java
@@ -4,26 +4,32 @@ package li.itcc.hackathon2014.vaduztour.hotcold;
 import li.itcc.hackathon2014.R;
 
 public enum DistanceHint {
-    COLD(R.raw.kalt_mp3),
-    WARM(R.raw.warm_mp3),
-    WARMER(R.raw.waermer_mp3),
-    WARMER_PROCEED(R.raw.waermer_wiiter_so_mp3),
-    HOT(R.raw.heiss_mp3),
-    HOTTER(R.raw.heisser_mp3),
-    HOTTER_PROCEED(R.raw.heisser_mp3),
-    ATDESTINATION(R.raw.ziel_erreicht_mp3),
-    COLDER(R.raw.kaelter_mp3),
-    TURN(R.raw.umtreia_mp3),
-    STEADY(R.raw.laufa_mp3);
+    COLD(R.raw.kalt_mp3, R.string.hotcold_hint_cold),
+    WARM(R.raw.warm_mp3, R.string.hotcold_hint_warm),
+    WARMER(R.raw.waermer_mp3, R.string.hotcold_hint_warner),
+    WARMER_PROCEED(R.raw.waermer_wiiter_so_mp3, R.string.hotcold_hint_warner_proceed),
+    HOT(R.raw.heiss_mp3, R.string.hotcold_hint_hot),
+    HOTTER(R.raw.heisser_mp3, R.string.hotcold_hint_hotter),
+    HOTTER_PROCEED(R.raw.heisser_wiiter_so_mp3, R.string.hotcold_hint_hotter_proceed),
+    AT_DESTINATION(R.raw.ziel_erreicht_mp3, R.string.hotcold_hint_at_destination),
+    COLDER(R.raw.kaelter_mp3, R.string.hotcold_hint_colder),
+    TURN(R.raw.umtreia_mp3, R.string.hotcold_hint_turn),
+    STEADY(R.raw.laufa_mp3, R.string.hotcold_hint_steady);
 
-    int mRessourceID;
+    private int mAudioHint;
+    private int mTextHint;
 
-    private DistanceHint(int distanceHint) {
-        mRessourceID = distanceHint;
+    private DistanceHint(int distanceHint, int textHint) {
+        mAudioHint = distanceHint;
+        mTextHint = textHint;
     }
 
-    public int getRessourceID() {
-        return mRessourceID;
+    public int getAudioHint() {
+        return mAudioHint;
+    }
+    
+    public int getTextHint() {
+        return mTextHint;
     }
 
 }
